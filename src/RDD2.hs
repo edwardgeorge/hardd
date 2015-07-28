e2ae9df05b74c5ca03b112b92da8984dc9e6d462
Edward George <edwardgeorge@gmail.com>
2015-07-28 22:13:18 +0100

    05

--- src/RDD2.hs
+++ src/RDD2.hs
@@ -2,6 +2,12 @@
 {-# LANGUAGE RankNTypes #-}
 module RDD2 where
 import Data.Foldable (toList)
+import Data.Functor.Identity
+import qualified Data.Map.Strict as M
+
+type HashFunc a = (a -> Int)
+type NumPartitions = Int
+type PartitionIndex = Int
 
 data RDD a where
   RDD :: Traversable f => (s -> f a) -> [s] -> RDD a
@@ -14,18 +20,32 @@ instance Functor RDD where
 produce :: [[a]] -> RDD a
 produce x = RDD (x !!) [0..length x - 1]
 
-compute :: RDD a -> Int -> Maybe [a]
+compute :: RDD a -> PartitionIndex -> Maybe [a]
 compute (RDD c n) i = fmap (toList . c) $ safeLookup n i
   where safeLookup []     _ = Nothing
         safeLookup (x:_)  0 = Just x
         safeLookup (_:xs) n = safeLookup xs (n - 1)
 
-mapPartitions :: Traversable g => (forall f. Traversable f => f a -> g b) -> RDD a -> RDD b
+mapPartitions :: Traversable g =>
+                 (forall f. Traversable f => f a -> g b)
+              -> RDD a -> RDD b
 mapPartitions f (RDD c n) = RDD (f . c) n
 
-mapPartitionsWithIndex :: Traversable g => (forall f. Traversable f => Int -> f a -> g b) -> RDD a -> RDD b
+mapPartitionsWithIndex :: Traversable g =>
+                          (forall f. Traversable f => PartitionIndex -> f a -> g b)
+                       -> RDD a -> RDD b
 mapPartitionsWithIndex f (RDD c n) = RDD go [0..length n - 1]
   where go i = f i . c $ n !! i
 
-getNumPartitions :: RDD a -> Int
+getNumPartitions :: RDD a -> NumPartitions
 getNumPartitions (RDD _ n) = length n
+
+reduceByKeyLocally :: Ord k => (v -> v -> v) -> KeyedRDD k v -> KeyedRDD k v
+reduceByKeyLocally f = mapPartitions (M.toList . go f)
+  where go f = flip foldr M.empty $ \(k, v) -> M.insertWith f k v
+
+keyByPartition :: HashFunc a -> NumPartitions -> RDD a -> KeyedRDD Int a
+keyByPartition f i = fmap $ \a -> (f a `mod` i, a)
+
+foldMapLocally :: Monoid b => (a -> b) -> RDD a -> RDD b
+foldMapLocally f = mapPartitions (Identity . foldMap f)
--- src/ShuffleFree.hs
+++ src/ShuffleFree.hs
@@ -4,12 +4,10 @@ import Control.Monad.Trans.Free
 
 import RDD2
 
-type HashFunc a = a -> Int
-type NumPartitions = Int
-
 data ShuffleF x where
   Collect     :: RDD a -> ([a] -> x) -> ShuffleF x
-  PartitionBy :: HashFunc a -> NumPartitions -> RDD a -> (RDD a -> x) -> ShuffleF x
+  PartitionBy :: HashFunc a -> NumPartitions -> RDD a -> (RDD a -> x)
+              -> ShuffleF x
 
 instance Functor ShuffleF where
   fmap f (Collect         r x) = Collect r (f . x)
@@ -20,5 +18,12 @@ type Shuffle = FreeT ShuffleF
 collect :: Monad m => RDD a -> Shuffle m [a]
 collect r = liftF $ Collect r id
 
-partitionBy :: Monad m => HashFunc a -> NumPartitions -> RDD a -> Shuffle m (RDD a)
+partitionBy :: Monad m =>
+               HashFunc a -> NumPartitions -> RDD a
+            -> Shuffle m (RDD a)
 partitionBy f n r = liftF $ PartitionBy f n r id
+
+foldMapRDD :: (Monad m, Monoid b) => (a -> b) -> RDD a -> Shuffle m b
+foldMapRDD f r = do
+  d <- collect $ foldMapLocally f r
+  return $ mconcat d
