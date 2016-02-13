9c78326c021a42bfe19b51aa8b0e63664f630adf
Edward George <edwardgeorge@gmail.com>
2016-02-13 14:22:37 +0000

    27

--- src/Class.hs
+++ src/Class.hs
@@ -1,11 +1,13 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE FunctionalDependencies #-}
+{-# LANGUAGE GADTs #-}
 {-# LANGUAGE KindSignatures #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE TypeOperators #-}
 module Class where
 import Data.Hashable (Hashable)  -- from: hashable
+import Data.Proxy
 import GHC.TypeLits
 
 import Exists
@@ -16,10 +18,20 @@ type PartitionIndex = Int
 type JoinKey a b = Either a b -> Exists Hashable
 type HashFunc a = a -> Exists Hashable
 
-type NumPartitions n = forall proxy. KnownNat n => proxy (n :: Nat)
+-- type NumPartitions n = forall proxy. KnownNat n => proxy (n :: Nat)
+--data NumPartitions n = forall proxy. KnownNat n => NumPartitions (proxy n)
+data NumPartitions (n :: Nat) where
+  NumPartitions :: KnownNat n => proxy n -> NumPartitions n
+
+numPartitions :: KnownNat n => NumPartitions n
+numPartitions = NumPartitions Proxy
 
 type PartitionMap a b = forall f. Traversable f => f a -> ExistsF Traversable b
 type IndexedPartitionMap a b = forall s. IsIndexable s => s -> PartitionMap a b
+
+-- ugh, need to pattern patch to bring the KnownNat constraint into scope
+natValP :: NumPartitions n -> Integer
+natValP (NumPartitions n) = natVal n
 
 class IsIndexable a where
   getPartitionIndex :: a -> PartitionIndex
